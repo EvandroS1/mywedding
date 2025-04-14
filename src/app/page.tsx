1e4d404bd9d710d624223e6a374893e683de16c2
Evandro <evandro.gomes@actionsys.com.br>
2025-04-14 20:16:39 -0300

    att pb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen relative">
       <Image src={"/assets/back.png"} fill={true} alt="convite"/>
-    <div className="flex flex-col relative w-full h-screen justify-end z-10 pb-20 text-gray-800 px-20">
+    <div className="flex flex-col relative w-full h-screen justify-end z-10 pb-32 text-gray-800 px-20">
       <Bar placeHolder="O nosso dia" link="/dia"/>
       <Bar placeHolder="O nosso lugar" link="/local"/>
       <Bar placeHolder="Nossa lista de presentes" link="/presentes"/>
