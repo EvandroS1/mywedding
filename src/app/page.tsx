ecff680c303692308dc20ffead60308dd8a6bcbc
Evandro <evandro.gomes@actionsys.com.br>
2025-04-14 19:28:00 -0300

    atualizado componeten bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import "@app/globals.css";
+// import "@app/globals.css";
 import Bar from "@/components/bar";
 
 export default function Home() {
@@ -7,10 +7,10 @@ export default function Home() {
     <>
       <Image src={"/assets/back.png"} fill={true} alt="convite"/>
     <div className="flex flex-col relative w-full h-screen pb-7 justify-end z-10 text-gray-800 px-20">
-      {Bar("O nosso dia", "/dia")}
-      {Bar("O nosso lugar", "/local")}
-      {Bar("Nossa lista de presentes", "/presentes")}
-      {Bar("Confirme sua presença", "/presenca")}
+      <Bar placeHolder="O nosso dia" link="/dia"/>
+      <Bar placeHolder="O nosso lugar" link="/local"/>
+      <Bar placeHolder="Nossa lista de presentes" link="/presentes"/>
+      <Bar placeHolder="Confirme sua presença" link="/presenca"/>
     </div>
     </>
   );
--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -1,16 +1,20 @@
-import '../../pages/globals.css';
-import { useRouter } from "next/router";
+"use client";
 
-const Bar = (placeHolder: string, link: string) => {
+import { useRouter } from "next/navigation";
 
+type BarProps = {
+  placeHolder: string;
+  link: string;
+};
+
+export default function Bar({ placeHolder, link }: BarProps) {
   const router = useRouter();
-return (
-  <div className='flex justify-center text-center items-center h-12 rounded-2xl shadow-inner w-full'>
-  <button onClick={() => router.push(link)}>
-  <p>{placeHolder}</p>
-  </button>
-</div>
-);
-} 
 
-export default Bar
+  return (
+    <div className='flex justify-center text-center items-center h-12 rounded-2xl shadow-xl w-full'>
+    <button onClick={() => router.push(link)}>
+    <p>{placeHolder}</p>
+    </button>
+  </div>
+  );
+}
