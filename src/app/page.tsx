8523b8a4313cf7243486fb034ed1e9106d1d47d9
Evandro <evandro.gomes@actionsys.com.br>
2025-04-14 20:12:06 -0300

    atualizando div da page principal, para a imagem de backgroud dar full cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Bar from "@/components/bar";
 
 export default function Home() {
   return (
-    <>
+    <div className="w-full h-screen relative">
       <Image src={"/assets/back.png"} fill={true} alt="convite"/>
     <div className="flex flex-col relative w-full h-screen pb-7 justify-end z-10 text-gray-800 px-20">
       <Bar placeHolder="O nosso dia" link="/dia"/>
@@ -12,6 +12,6 @@ export default function Home() {
       <Bar placeHolder="Nossa lista de presentes" link="/presentes"/>
       <Bar placeHolder="Confirme sua presença" link="/presenca"/>
     </div>
-    </>
+    </div>
   );
 }
