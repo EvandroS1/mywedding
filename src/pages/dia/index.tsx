bb74aafb0dc8ce0cc317415927c80cc019978d81
Evandro <evandro.gomes@actionsys.com.br>
2025-04-14 21:16:47 -0300

    atualizado texto do li

--- src/pages/dia/index.tsx
+++ src/pages/dia/index.tsx
@@ -4,13 +4,13 @@ import "../../app/globals.css";
 const Dia = () => {
   return (
     <Container picture="/assets/evandro melissa.jpg">
-      <div className="text-center p-6 bg-white rounded-lg shadow-md max-w-md mx-auto">
+      <div className="text-center p-6 bg-white rounded-lg shadow-2xl max-w-md mx-auto">
   <h1 className="text-2xl font-semibold mb-4">Data: 07/09/2025</h1>
   <ul className="space-y-3 text-lg text-gray-700">
     <li>O evento começa às <strong>16:30</strong></li>
     <li>Entrada permitida até as <strong>17:00</strong></li>
     <li>Encerramento às <strong>22:00</strong></li>
-    <li>Não será permitido a entrada de terceiro, apenas constantes no convite.</li>
+    <li>Não será permitido a entrada de terceiro, apenas presentes no convite.</li>
     <li>Por favor, cuidem com carinho: danos ao espaço ou à decoração poderão ser cobrados.</li>
   </ul>
 </div>
