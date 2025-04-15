0ffdc3f3fc2287a54c2334b13e945f690269effb
Evandro <evandro.gomes@actionsys.com.br>
2025-04-15 10:13:44 -0300

    atualizando para o button todo fique clicavel, não apenas o nome.

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -11,10 +11,8 @@ export default function Bar({ placeHolder, link }: BarProps) {
   const router = useRouter();
 
   return (
-    <div className='flex justify-center text-center items-center h-12 rounded-2xl shadow-xl w-full'>
-    <button onClick={() => router.push(link)}>
+    <div className='flex justify-center text-center items-center h-12 rounded-2xl shadow-xl w-full' onClick={() => router.push(link)}>
     <p>{placeHolder}</p>
-    </button>
   </div>
   );
 }
