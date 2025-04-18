56de087f79ad916958238913656b7245b4226bfc
Evandro <evandro.gomes@actionsys.com.br>
2025-04-18 10:48:47 -0300

    Adicionado clearErrors

--- src/pages/presenca/index.tsx
+++ src/pages/presenca/index.tsx
@@ -6,6 +6,7 @@ import { useForm, useFieldArray } from "react-hook-form";
 import { MinusCircle, PlusCircle } from "@geist-ui/icons";
 import { toast, ToastContainer } from "react-toastify";
 import { useEffect, useState } from "react";
+import { Loading } from "@geist-ui/react";
 
 interface IConvidado {
   nome: string;
@@ -19,10 +20,28 @@ interface IFormData {
 const Presenca = () => {
   const [convidados, setConvidados] = useState<IConvidado[]>([]);
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  // const [filtro, setFiltro] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   const [sugestoes, setSugestoes] = useState<string[]>([]);
+  const {
+    register,
+    control,
+    handleSubmit,
+    setValue,
+    setError,
+    formState: { errors },
+    clearErrors,
+    watch,
+  } = useForm<IFormData>({
+    defaultValues: {
+      convidados: [{ nome: "", confirmado: false }],
+    },
+  });
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "convidados",
+  });
+console.log('errors', errors)
   const getConvidados = async () => {
     try {
       const response = await fetch(
@@ -39,22 +58,16 @@ const Presenca = () => {
     getConvidados();
   }, []);
 
-  const { register, control, handleSubmit, setValue, setError } =
-    useForm<IFormData>({
-      defaultValues: {
-        convidados: [{ nome: "", confirmado: false }],
-      },
-    });
-
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "convidados",
-  });
+  const inputValue = watch("convidados.0.nome");
+  useEffect(() => {
+    console.log("inputValue", inputValue);
+  }, [inputValue]);
 
   const handleSearch = (
     e: React.ChangeEvent<HTMLInputElement>,
     index: number
   ) => {
+    clearErrors("convidados");
     const valor = e.target.value;
     setActiveIndex(index);
 
@@ -77,6 +90,7 @@ const Presenca = () => {
   };
 
   const onSubmit = async (data: IFormData) => {
+    console.log("foi");
     for (const [index, convidado] of data.convidados.entries()) {
       const foiConvidado = convidados.some(
         (c) => c.nome?.toLowerCase() === convidado.nome?.toLowerCase()
@@ -84,27 +98,31 @@ const Presenca = () => {
       // const estaConfirmado = convidados.find(
       //   (c) => c.nome?.toLowerCase() === convidado.nome?.toLowerCase()
       // );
-  
+
       if (!foiConvidado) {
         setError("convidados", {
           types: {
             required: "Esta pessoa não está na lista de convidados",
           },
         });
-        toast.error(`${convidado.nome} não está na lista de convidados!`, { theme: "dark" });
+        toast.error(`${convidado.nome} não está na lista de convidados!`, {
+          theme: "dark",
+        });
         setValue(`convidados.${index}.nome`, "");
+        console.log("if do não esta confirmado");
         continue;
       }
-  
+      console.log("passou o if do não esta confirmado");
       // if (estaConfirmado?.confirmado) {
       //   toast.error(`${estaConfirmado.nome} já está confirmado!`, { theme: "dark" });
       //   setValue(`convidados.${index}.nome`, "");
       //   continue;
       // }
-  
+
       convidado.confirmado = true;
-  
+
       try {
+        setLoading(true);
         const response = await fetch(
           "https://67fffe04b72e9cfaf72687d9.mockapi.io/api/convidados/nome",
           {
@@ -115,25 +133,31 @@ const Presenca = () => {
             body: JSON.stringify(convidado),
           }
         );
-  
+
         if (response.ok) {
-          toast.success(`${convidado.nome} confirmado com sucesso!`, { theme: "colored" });
+          toast.success(`${convidado.nome} confirmado com sucesso!`, {
+            theme: "colored",
+          });
           remove(index);
+          setLoading(false);
         } else {
-          toast.error(`Erro ao confirmar ${convidado.nome}.`, { theme: "dark" });
+          toast.error(`Erro ao confirmar ${convidado.nome}.`, {
+            theme: "dark",
+          });
         }
       } catch (error) {
         console.error("Erro:", error);
-        toast.error(`Erro de rede ao confirmar ${convidado.nome}.`, { theme: "dark" });
+        toast.error(`Erro de rede ao confirmar ${convidado.nome}.`, {
+          theme: "dark",
+        });
       }
     }
-  
+
     // Se quiser resetar o primeiro campo se sobrar só ele vazio
     if (fields.length === 1) {
       setValue("convidados.0.nome", "");
     }
   };
-  
 
   return (
     <Container picture="/assets/alianca.jpg">
@@ -153,6 +177,7 @@ const Presenca = () => {
                   className="border-solid px-4 py-2 border rounded-lg border-[#c89857] shadow-xl"
                   {...register(`convidados.${index}.nome`, { required: true })}
                   placeholder="Digite o nome"
+                  style={errors.convidados ? { border: "1px solid #bf1650" } : {}}
                   onFocus={() => setActiveIndex(index)}
                   onChange={(e) => handleSearch(e, index)}
                 />
@@ -189,9 +214,9 @@ const Presenca = () => {
 
             <button
               type="submit"
-              className="h-10 shadow-xl rounded-lg text-white hover:bg-green-700 w-full bg-green-600"
+              className="h-10 shadow-xl rounded-lg flex justify-center items-center text-white hover:bg-green-700 w-full bg-green-600"
             >
-              Confirmar
+              {loading ? <Loading color="white" /> : "Confirmar"}
             </button>
           </form>
         </div>
