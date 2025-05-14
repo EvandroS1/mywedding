b9ada30da998b3b541d29f3c300e11106fcb7ec9
Evandro <evandro.gomes@actionsys.com.br>
2025-05-14 16:25:06 -0300

    atualizado webhook

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,27 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    // Aqui você pode tratar os eventos, como:
-    // if (req.body.type === "payment" && req.body.action === "payment.created") { ... }
     try {
-      fetch(
-        "https://6823ff4b65ba058033988478.mockapi.io/payments",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(req.body),
-        }
-      );
-    } catch {
-      console.error("Error fetching");
-    }
+      const response = await fetch("https://6823ff4b65ba058033988478.mockapi.io/payments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body),
+      });
 
-    res.status(200).json({ received: true });
+      if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.status}`);
+      }
+
+      res.status(200).json({ received: true });
+    } catch (err) {
+      console.error("Erro ao enviar para MockAPI:", err);
+      res.status(500).json({ error: 'Erro ao enviar para o banco' });
+    }
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
 }
+
