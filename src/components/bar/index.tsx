72ff6d74ce540ead5c96d04990d7d6b5bd52f2a7
Evandro <evandro.gomes@actionsys.com.br>
2025-05-26 21:48:48 -0300

    atualizado bar

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -11,7 +11,7 @@ export default function Bar({ placeHolder, link }: BarProps) {
   const router = useRouter();
 
   return (
-    <div className='flex justify-center border text-center items-center h-16  rounded-2xl shadow-xl w-full' onClick={() => router.push(link)}>
+    <div className='flex justify-center text-center items-center h-12  rounded-2xl shadow-xl w-full' onClick={() => router.push(link)}>
     <p className="m-0">{placeHolder}</p>
   </div>
   );
